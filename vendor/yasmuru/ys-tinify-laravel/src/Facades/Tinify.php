8789ae7bc129d4c440e416ad9d7919e0445363fe
jianghaonan <jianghaonan0130@163.com>
2019-08-06 15:46:41 +0800

    稳定版v1

--- vendor/yasmuru/ys-tinify-laravel/src/Facades/Tinify.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace yasmuru\LaravelTinify\Facades;
-use Illuminate\Support\Facades\Facade;
-class Tinify extends Facade {
-	protected static function getFacadeAccessor(){
-		return 'tinify';
-	}
-}
